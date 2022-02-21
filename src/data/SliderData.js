035a0f6ba453cd45bdb131a0ccf0f5b1479324dd
Shardul-2323 <shardulmore2323@gmail.com>
2022-02-21 10:11:38 +0530

    initial commit

---
+++ src/data/SliderData.js
@@ -0,0 +1,44 @@
+import ImageOne from '../images/house1.jpg';
+import ImageTwo from '../images/house2.jpg';
+import ImageThree from '../images/house3.jpg';
+import ImageFour from '../images/house4.jpg';
+
+
+export const SliderData=[
+    {
+        title:'Luxury villa in indonesia',
+        price:'rs.4200000',
+        path:"/homes",
+        label:"view home",
+        image:ImageOne,
+        alt:'house'
+
+    },
+    {
+        title:'Mannat villa in mumbai',
+        price:'rs.6200000',
+        path:"/homes",
+        label:"view home",
+        image:ImageTwo,
+        alt:'house'
+
+    },
+    {
+        title:'Tirthroop in Kolhapur',
+        price:'rs.14200000',
+        path:"/homes",
+        label:"view home",
+        image:ImageThree,
+        alt:'house'
+
+    },
+    {
+        title:'Anand in delhi',
+        price:'rs.5200000',
+        path:"/homes",
+        label:"view home",
+        image:ImageFour,
+        alt:'house'
+
+    }
+];
